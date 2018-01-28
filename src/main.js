f27850d81a9bb55524a6241014e6420d8c7054c1
Gabriel Théron <gabe90atwork@gmail.com>
2018-01-28 13:10:31 +0100

    Ajoute le scaling

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ class Game extends Phaser.Game {
   }
 }
 
-window.game = new Game()
+window.game = new Game();
 
 if (window.cordova) {
   var app = {
--- src/states/Boot.js
+++ src/states/Boot.js
@@ -20,7 +20,18 @@ export default class extends Phaser.State {
     text.anchor.setTo(0.5, 0.5);
 
     this.load.image('loaderBg', './assets/images/loader-bg.png')
-    this.load.image('loaderBar', './assets/images/loader-bar.png')
+    this.load.image('loaderBar', './assets/images/loader-bar.png');
+
+    this.game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+    this.game.scale.setUserScale(4, 4);
+
+// enable crisp rendering
+    this.game.renderer.renderSession.roundPixels = true;
+    Phaser.Canvas.setImageRenderingCrisp(this.game.canvas);
+
+    //this.game.world.setBounds(0, 0, 2000, 2000);
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.setMinMax(640, 360, 1280, 720);
   }
 
   render () {
